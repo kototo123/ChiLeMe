aac70e62761ad33856db32789983661ec24f6184
kototo123 <kototo123@users.noreply.github.com>
2026-06-19 14:29:44 +0800

    Disable Redis auto-configuration

--- backend/src/main/java/com/chileme/ChileMeApplication.java
+++ backend/src/main/java/com/chileme/ChileMeApplication.java
@@ -3,9 +3,11 @@ package com.chileme;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
 @EnableScheduling
 @MapperScan("com.chileme.mapper")
 public class ChileMeApplication {
